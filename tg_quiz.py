85e93bd3ed1601e67867920cce94b584a73d3de6
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-03-08 14:33:25 +0300

    Corrected the variable name

--- tg_quiz.py
+++ tg_quiz.py
@@ -1,5 +1,4 @@
 import os
-import json
 import redis
 import logging
 import telegram
--- vk_quiz.py
+++ vk_quiz.py
@@ -1,5 +1,4 @@
 import os
-import json
 import redis
 import logging
 import vk_api as vk
@@ -9,6 +8,9 @@ from random import choice
 from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+from create_dictionary_questions_answers import get_questions_answer
+
 
 logger = logging.getLogger(__file__)
 
@@ -23,9 +25,9 @@ def handle_new_question_request(event, vk_api, question, redis, keyboard):
     )
 
 
-def handle_solution_attempt(event, vk_api, quiz_text, redis, keyboard):
+def handle_solution_attempt(event, vk_api, questions_answer, redis, keyboard):
     question = redis.get(event.user_id).decode("utf-8")
-    answer = quiz_text[question]
+    answer = questions_answer[question]
     if event.text == answer:
         text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос».'
     else:
@@ -38,9 +40,9 @@ def handle_solution_attempt(event, vk_api, quiz_text, redis, keyboard):
     )
 
 
-def handle_show_correct_answer(event, vk_api, quiz_text, redis, keyboard):
+def handle_show_correct_answer(event, vk_api, questions_answer, redis, keyboard):
     question = redis.get(event.user_id).decode("utf-8")
-    answer = quiz_text[question]
+    answer = questions_answer[question]
     vk_api.messages.send(
         peer_id=event.user_id,
         random_id=get_random_id(),
@@ -49,7 +51,7 @@ def handle_show_correct_answer(event, vk_api, quiz_text, redis, keyboard):
     )
 
 
-def command_handler(vk_token, quiz_text):
+def add_keyboard_and_process_commands(vk_token, questions_answer):
     keyboard = VkKeyboard(one_time=True)
     keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button('Сдаться', color=VkKeyboardColor.PRIMARY)
@@ -58,22 +60,22 @@ def command_handler(vk_token, quiz_text):
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
-        question = choice(list(quiz_text))
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             if event.text == 'Новый вопрос':
+                question = choice(list(questions_answer))
                 handle_new_question_request(
                     event, vk_api, question,
                     redis, keyboard
                 )
             elif event.text == 'Сдаться':
                 handle_show_correct_answer(
-                    event, vk_api, quiz_text,
+                    event, vk_api, questions_answer,
                     redis, keyboard
                 )
             else:
                 handle_solution_attempt(
                     event, vk_api,
-                    quiz_text, redis,
+                    questions_answer, redis,
                     keyboard
                 )
 
@@ -91,6 +93,5 @@ if __name__ == "__main__":
         password=database_password
     )
 
-    with open('questions_answer.json', 'r') as quiz_file:
-        quiz_text = json.load(quiz_file)
-    command_handler(vk_token, quiz_text)
+    questions_answer = get_questions_answer()
+    add_keyboard_and_process_commands(vk_token, questions_answer)
